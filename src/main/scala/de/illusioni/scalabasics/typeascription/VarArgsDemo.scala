00d07ea6176d601f1b753521a6d10ab8c2f3c7db
Klaus Baumgartner <klaus@illusioni.de>
2021-12-12 23:12:07 +0100

    added VarArgsDemo

---
+++ src/main/scala/de/illusioni/scalabasics/typeascription/VarArgsDemo.scala
@@ -0,0 +1,16 @@
+package de.illusioni.scalabasics.typeascription
+
+object VarArgsDemo {
+
+  def main(args: Array[String]): Unit = {
+    val x : Seq[Seq[Int]] = Seq(Seq(1),Seq(2))
+
+    def f(arg: Seq[Any]*) : Int = {
+      println(s"argument(s): $arg")
+      arg.length
+    }
+    println(f(x)) //1 as x is taken as single arg
+    println(f(x:_*))  // 2 as x is "unpacked" as a Seq[Any]*
+  }
+
+}
