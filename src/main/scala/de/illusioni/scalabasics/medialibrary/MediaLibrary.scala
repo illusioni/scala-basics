f40a1f7f75854909953c6740e7efbcec3df7228a
Klaus Baumgartner <klaus@illusioni.de>
2022-01-18 13:52:41 +0000

    example code fragments while explaining

--- src/main/scala/de/illusioni/scalabasics/medialibrary/MediaLibrary.scala
+++ src/main/scala/de/illusioni/scalabasics/medialibrary/MediaLibrary.scala
@@ -1,46 +1,46 @@
 package de.illusioni.scalabasics.medialibrary
 
 case class MediaLibrary(items: Set[Content] = Set()) extends Collection[Content] {
-  override def :+(newItem: Content): MediaLibrary = (super.:+(newItem)).asInstanceOf[MediaLibrary]
+//  override def :+(newItem: Content): MediaLibrary = (super.:+(newItem)).asInstanceOf[MediaLibrary]
 }
 
 object MediaLibrary {
 
   def main(args: Array[String]): Unit = {
 
-    val movie = Movie("Wag the Dog", Comedy, "Barry Levinson", Some(Media(Video, "/tmp/wag-the-dog.avi")))
-//    if (movie.media.nonEmpty) {
-//      println(movie.media.get)
+//    val movie = Movie("Wag the Dog", Comedy, "Barry Levinson", Some(Media(Video, "/tmp/wag-the-dog.avi")))
+////    if (movie.media.nonEmpty) {
+////      println(movie.media.get)
+////    }
+//
+//    // movie.media.toString => never throws NullPointerException
+//
+//    println(movie)
+//    movie.media match {
+//      case Some(m) => println(s"the content has a media file of kind ${m.kind}: ${m.path}")
+//      case None => println("the content has no media file associated with it")
 //    }
 
-    // movie.media.toString => never throws NullPointerException
-
-    println(movie)
-    movie.media match {
-      case Some(m) => println(s"the content has a media file of kind ${m.kind}: ${m.path}")
-      case None => println("the content has no media file associated with it")
-    }
 
 
 
+    val lost = Series("Lost", Action)
 
-//    val lost = Series("Lost", Action)
-//
-//    val tatort = Series(
-//      "Der Tatort",
-//      Crime,
-//      List(
-//        Episode(1, 1),
-//        Episode(1, 2),
-//        Episode(2, 1),
-//        Episode(2, 2),
-//        Episode(2, 3)
-//      )
-//    )
-//
-//    var myLib = MediaLibrary(lost, tatort)
-//    myLib = myLib :+ Movie("Wag the Dog", Comedy, "Barry Levinson")
-//    println(myLib)
+    val tatort = Series(
+      "Der Tatort",
+      Crime,
+      List(
+        Episode(1, 1),
+        Episode(1, 2),
+        Episode(2, 1),
+        Episode(2, 2),
+        Episode(2, 3)
+      )
+    )
+
+    var myLib: Collection[Content] = MediaLibrary(lost, tatort)
+    myLib = myLib :+ Movie("Wag the Dog", Comedy, "Barry Levinson")
+    println(myLib)
   }
 
   def apply(items: Content*) = new MediaLibrary(Set(items: _*))
