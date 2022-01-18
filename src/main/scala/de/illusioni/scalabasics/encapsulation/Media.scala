c239574995bbaf5266359b33de33263353387d78
Klaus Baumgartner <klaus@illusioni.de>
2022-01-18 10:36:45 +0000

    [tmp]

---
+++ src/main/scala/de/illusioni/scalabasics/encapsulation/Media.scala
@@ -0,0 +1,20 @@
+package de.illusioni.scalabasics.encapsulation
+
+import java.nio.file.Path
+
+class Media(var path: String) {
+  private var fileRef: Path = Path.of(path)
+
+  def path_=(newVal: String) = {
+    fileRef = Path.of(newVal)
+  }
+
+}
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val m = new Media ("old/path")
+    m.path = "new/path"
+  }
+}
--- src/test/scala/de/illusioni/scalabasics/shapes/ShapesSpec.scala
+++ src/test/scala/de/illusioni/scalabasics/shapes/ShapesSpec.scala
@@ -6,13 +6,14 @@ import org.scalatest.matchers.should
 
 import scala.language.postfixOps
 
-class ShapesSpec extends AnyFlatSpec with GivenWhenThen with should.Matchers {
+class ShapesSpec extends AnyFlatSpec with should.Matchers {
 
   val rectanglesWithArea = Map(
     Rectangle(2, 5) -> 10,
     Rectangle(1, 1) -> 1,
     Rectangle (0, 7) -> 0,
-//    Square(5) -> 25
+    Square(5) -> 25,
+    Square(3) -> 9
   )
 
   "A rectangle" should "compute its surface area correctly" in {
@@ -20,7 +21,23 @@ class ShapesSpec extends AnyFlatSpec with GivenWhenThen with should.Matchers {
   }
 
   "A rectangle" should "resize when setter of width is called" in {
-    val myRect = Rectangle(2, 5)
-    myRect.w = 4
+    // val myRect = Rectangle(2, 5)
+    for ((rect, area) <- rectanglesWithArea) {
+      rect.setWidth(4)
+      rect.setHeight(5)
+      rect.area should equal(20)
+    }
   }
+
+  "Squares" should "resize both sides when setter is called" in {
+    for ((square, area) <- rectanglesWithArea) {
+      if (square.isInstanceOf[Square]) {
+        square.setWidth(7)
+        square.height should equal(7)
+        square.setHeight(9)
+        square.width should equal(9)
+      }
+    }
+  }
+
 }
