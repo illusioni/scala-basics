2257cc5cad4753b0d7aed7fa9cfa069c4da5cf15
Klaus Baumgartner <klaus@illusioni.de>
2022-01-18 13:08:32 +0000

    java pendant

--- src/main/scala/de/illusioni/scalabasics/medialibrary/Media.scala
+++ src/main/scala/de/illusioni/scalabasics/medialibrary/Media.scala
@@ -1,6 +1,24 @@
 package de.illusioni.scalabasics.medialibrary
 
-case class Media(kind: Kind, path: String)
+import java.nio.file.Path
+
+case class Media(kind: Kind, path: String) {
+  private val fileRef = Path.of(path)
+}
+//Das Java Pendant wäre:
+//public class Media {
+//  public final Kind kind;
+//  public final String path;
+//  private final Path fileRef;
+//
+//  public Media(Kind kind, String path) {
+//    this.kind = kind;
+//    this.path = path;
+//    fileRef = Path.of(path)
+//  }
+//}
+
+
 
 sealed trait Kind
 case object Video extends Kind
